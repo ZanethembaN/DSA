9fd1c9989377c764cf34ba6740ab8234beda8cb1
Zanethemba <zngwenya023@student.wethinkcode.co.za>
2024-12-11 14:30:49 +0200

    added docs

--- src/main/java/portfolio/MergeSortedArray.java
+++ src/main/java/portfolio/MergeSortedArray.java
@@ -2,12 +2,22 @@ package portfolio;
 
 public class MergeSortedArray {
 
-    public void merge(int[] nums1, int m, int[] nums2, int n){
 
+    /**
+     * Merges two sorted arrays into the first array in sorted order.
+     *
+     * @param nums1 the first sorted array with extra space at the end
+     * @param m     the number of elements in nums1
+     * @param nums2 the second sorted array
+     * @param n     the number of elements in nums2
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n){
+        // Pointers for nums1, nums2, and the merged array
         int i = m - 1;
         int j = n - 1;
         int k = m + n - 1;
 
+        // Merge arrays from the back
         while (i >= 0 && j >= 0){
             if (nums1[i] > nums2[j]){
                 nums1[k--] = nums1[i--];
@@ -17,6 +27,7 @@ public class MergeSortedArray {
             }
         }
 
+        // Add remaining elements from nums2, if any
         while (j >= 0){
             nums1[k--] = nums2[j--];
         }
